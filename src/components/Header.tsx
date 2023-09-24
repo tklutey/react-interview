cfc2c521f8ae4d54a5c9dc0cfa27119813543d25
Tucker Klutey <tuckerklutey@Tuckers-Air.lan>
2023-09-24 11:24:56 -0400

    Adding basic styling example

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,8 @@
+import {Box, styled} from "@mui/material";
 
 export default function Header() {
-    return <h1> This is the main page. </h1>
+    const StyledBox = styled(Box)({
+        backgroundColor: 'blue'
+    });
+    return <StyledBox> This is the main page. </StyledBox>
 }

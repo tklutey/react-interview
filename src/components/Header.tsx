2f30e3f8da487816d184541c339058b57256d15c
Tucker Klutey <tuckerklutey@Tuckers-Air.lan>
2023-09-26 12:40:38 -0400

    Adding basic API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
 import {Box, styled} from "@mui/material";
+import {useQuery} from "@tanstack/react-query";
+import axios from 'axios';
 
 export default function Header() {
     const StyledBox = styled(Box)({
-        backgroundColor: 'blue'
+        backgroundColor: 'lightblue'
     });
-    return <StyledBox> This is the main page. </StyledBox>
+    const { data } = useQuery(['test'], () =>
+        axios
+            .get('/api/example')
+            .then((res) => res.data)
+    )
+    return <StyledBox> {data ? data['message'] : 'No data found'} </StyledBox>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Header";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
 
 export default function IndexPage() {
+    const queryClient = new QueryClient()
+
     return (
-        <main>
-            <div>
-                <Header />
-            </div>
-        </main>
+        <QueryClientProvider client={queryClient}>
+            <main>
+                <div>
+                    <Header />
+                </div>
+            </main>
+        </QueryClientProvider>
     )
 }
